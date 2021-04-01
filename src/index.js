24a781cebe37cd83fcb7dfecdb1ccf52a8b37f99
sb-gravity100 <sbducao0@gmail.com>
2021-04-02 05:27:30 +0800

    Done for the DAY!!!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-import fetch from 'node-fetch'
-import cheerio from 'cheerio'
-import { VidstreamingScraper } from 'vidstreaming-scraper'
+import cheerio from 'cheerio';
+import axios from 'axios';
 
-console.log('Done')
+const url = 'https://gogo-stream.com';
+const search = 'bakemonogatari';
+const getList = async link => {
+   const { data } = await axios.get(link, {
+      headers: { 'X-Requested-With': 'XMLHttpRequest' },
+      baseURL: url,
+   });
+   const $ = cheerio.load(data);
+   const list = $('.listing.items.lists .video-block a');
+   return list
+};
+const fetchAnime = async search => {
+   const anime = await axios.get('/ajax-search.html', {
+      headers: { 'X-Requested-With': 'XMLHttpRequest' },
+      params: {
+         keyword: search,
+      },
+      baseURL: url,
+   });
+   const $ = cheerio.load(anime.data.content);
+   const link = $('ul a').attr('href')
+   const list = await getList(link)
+   console.log(list)
+};
+
+fetchAnime(search);
