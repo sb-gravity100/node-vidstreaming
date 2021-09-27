f446c3bcfc9bc2aa6f3c840676828ee2f3e4e266
Sb <sbducao0@gmail.com>
2021-09-27 20:49:42 +0000

    Done

--- test/index.js
+++ test/index.js
@@ -1,3 +1,35 @@
-const { format } = require('../dist/funcs/filter_string');
+const Vidstreaming = require('../dist');
+const { FCDN } = require('../dist/classes/mirrors');
+const _ = require('lodash');
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-console.log(format('R 3 4 5', 10));
+inquirer.registerPrompt('search-list', require('inquirer-search-list'));
+
+process.argv.splice(0, 2);
+
+async function boot() {
+   console.log(process.argv[0]);
+   const searchRes = await Vidstreaming.term(process.argv[0]);
+   const { index } = await inquirer.prompt([
+      {
+         name: 'index',
+         type: 'search-list',
+         message: 'Choose anime',
+         choices: searchRes.map((e, i) => ({
+            name: e.title,
+            value: i,
+         })),
+      },
+   ]);
+   const anime = searchRes[index];
+   const eps = await anime.getEpisodes();
+   const sources = await Promise.all(eps.map((e) => e.getSources()));
+   var HDP = sources.map((e) => e.find((f) => f.name === 'HDP'));
+   await fs.promises.writeFile(
+      __dirname + '/' + _.snakeCase(anime.title) + '.txt',
+      HDP.map((e) => e.links).join('\n')
+   );
+}
+
+boot().catch(console.log);
